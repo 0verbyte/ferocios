1808ddea86203ed2aa683704ae3d2e309ba99e7d
Morten Kristensen <me@mortens.dev>
2020-08-05 08:02:58 +0200

    Bootstrap checks for QEMU
    It is needed when running the kernel.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,21 @@
 rustup +nightly component add rust-src llvm-tools-preview
 cargo install bootimage
 cargo +nightly check
+
+# Takes program as argument.
+checkProgram() {
+  if hash $1 2> /dev/null; then
+    echo "$1 is installed!"
+  else
+    echo "$1 is not installed!"
+    exit 1
+  fi
+}
+
+# Takes title as argument.
+writeBanner() {
+  echo "== $1 =="
+}
+
+writeBanner "QEMU"
+checkProgram qemu-system-x86_64
