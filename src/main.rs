4e207ad9358159d49355ec5ca958fe6d461de1bd
Brandon Davis <dbrandon.91@gmail.com>
2020-09-05 09:13:17 -0700

    Add basic custom test runner

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success)
+}
 
 use core::panic::PanicInfo;
 
 extern crate rlibc;
 
+mod serial;
 mod vga;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     eprintln!("{}", info);
@@ -14,8 +28,40 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+
+    loop {}
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("FerociOS booting..");
+
+    #[cfg(test)]
+    test_main();
+
     panic!("Not implemented");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+const QEMU_IOBASE: u16 = 0xf4;
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(QEMU_IOBASE);
+        port.write(exit_code as u32)
+    }
 }
